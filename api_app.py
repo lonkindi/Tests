b86ac83761a56d85261c3f9875b8046028d497c8
lonkindi <legions.ru@gmail.com>
2020-04-08 23:01:39 +0500

    Task2 begin

--- api_app.py
+++ api_app.py
@@ -1,4 +1,5 @@
 import requests
+import requests.exceptions
 
 API_KEY = 'trnsl.1.1.20190712T081241Z.0309348472c8719d.0efdbc7ba1c507292080e3fbffe4427f7ce9a9f0'
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
@@ -10,14 +11,13 @@ def translate_it(s_text, a_key=API_KEY, a_url=URL):
         'text': s_text,
         'lang': '{}-{}'.format('en', 'ru'),
     }
-    try:
-        response = requests.get(a_url, params=params)
-        return response
-    except Exception as err:
-        print(err.__str__())
-        return requests.exceptions.MissingSchema.__name__
+    # try:
+    response = requests.get(a_url, params=params)
+    return response
+    # except requests.exceptions.MissingSchema as err:
+    #     return err
 
 
 
 if __name__ == '__main__':
-    print(translate_it('Hi!', API_KEY, 'URL'))
+    print(translate_it('A', 'B', 'C', 'D'))
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,7 @@
 import unittest
 import api_app
 import requests
+import requests.exceptions
 from io import StringIO
 from unittest.mock import patch
 
@@ -11,19 +12,21 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(output['text'][0], 'Привет!')
         self.assertEqual(output['code'], 200)
 
-    # def test_wrong_args(self):
-    #     self.assertRaises(TypeError, api_app.translate_it())
-        # self.assertRaises(TypeError, api_app.translate_it('A', 'B'))
-        # self.assertRaises(TypeError, api_app.translate_it('A', 'B', 'C', 'D'))
-        # self.assertRaises(TypeError, api_app.translate_it('#'))
+    def test_wrong_args(self):
+        # self.assertRaises(TypeError, api_app.translate_it())
+    #     self.assertRaises(TypeError, api_app.translate_it('A', 'B'))
+        self.assertRaises(TypeError, api_app.translate_it('A', 'B', 'C', 'D'))
+    #     self.assertRaises(TypeError, api_app.translate_it('#'))
 
     def test_connection(self):
         r_code = api_app.translate_it('Hi!', 'API_KEY', api_app.URL).status_code
         self.assertEqual(r_code, 403)
-        # r_code = api_app.translate_it('Hi!', api_app.API_KEY, 'URL')
-        # print('r_code=', r_code)
-        r_err = requests.exceptions.MissingSchema
-        self.assertRaises(requests.exceptions.MissingSchema, api_app.translate_it('Hi!', api_app.API_KEY, 'URL'))
+        # try:
+        #         #     api_app.translate_it('Hi!', api_app.API_KEY, 'URL')
+        #         # except Exception as r_err:
+        #         #     print('r_err=', r_err)
+
+        # self.assertRaises(requests.exceptions.MissingSchema, api_app.translate_it('Hi!', api_app.API_KEY, 'URL'))
 
 
 
